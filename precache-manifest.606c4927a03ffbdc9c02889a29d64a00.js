5414e4c696ed11bdccce9196c5fa81870f03a535
APPX01 <bhushan.patil@appxtechnologies.com>
2019-11-27 18:53:45 +0530

    Updates

---
+++ precache-manifest.606c4927a03ffbdc9c02889a29d64a00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611753ccaefb92a682516e8b7f0f6282",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "4da0c2d2c860184f2ad5",
+    "url": "/Monsters-Rolodex/static/css/main.32b3f0ff.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/Monsters-Rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "4da0c2d2c860184f2ad5",
+    "url": "/Monsters-Rolodex/static/js/main.0d07194e.chunk.js"
+  },
+  {
+    "revision": "11e2dea3cac39d985a42",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.3ef3660b.js"
+  }
+]);
